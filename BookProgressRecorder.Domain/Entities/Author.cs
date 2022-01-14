35261ffb212c05868da6bd7246e00fc6a7f3ac09
Bruno Pereira da Silva <brunpersilva@gmail.com>
2022-01-14 00:37:46 -0300

    Created initial domain.

---
+++ BookProgressRecorder.Domain/Entities/Author.cs
@@ -0,0 +1,10 @@
+﻿namespace BookProgressRecorder.Domain.Entities
+{
+    public class Author
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+    }
+}
---
+++ BookProgressRecorder.Domain/Entities/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace BookProgressRecorder.Domain.Entities
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Author Author { get; set; }
+        public DateTime PublishDate { get; set; }
+        public int TotalPages { get; set; }
+        public int CurrentPage { get; set; }
+    }
+}
